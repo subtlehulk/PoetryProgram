d8ab421dda5d2975f358479d0d3d843db158a704
subtlehulk <subtlehulk@gmail.com>
2013-12-20 10:12:12 +0000

    Poem class
    I have hard coded all the poems that will be available in this class file.

---
+++ PoemClass.py
@@ -0,0 +1,290 @@
+import sys
+class Poem:
+    def idkwts_poem(self):
+        print('''
+        I remember a time when I would
+        churn out love poems like a chef would
+        churn out meals,
+        My notebooks were never empty,
+        my pens always out of ink,
+        But now, I can never start a poem without
+        frustration, hitting my head like a hammer to
+        soft brick,
+        I find it even harder to finish.
+        I have all of these thoughts in my head,
+        conversations I've had with myself that I had
+        to write down,
+        But it never feels the same.
+        I've swapped my pen for a pencile because I don't
+        take my writing seriously anymore.
+        All I wanna do is tell you what I tell you every time
+        I see you, 
+        But in a way that makes it special every time I do.
+        ''')
+    def idkwts(self):
+        return('''
+        I remember a time when I would
+        churn out love poems like a chef would
+        churn out meals,
+        My notebooks were never empty,
+        my pens always out of ink,
+        But now, I can never start a poem without
+        frustration, hitting my head like a hammer to
+        soft brick,
+        I find it even harder to finish.
+        I have all of these thoughts in my head,
+        conversations I've had with myself that I had
+        to write down,
+        But it never feels the same.
+        I've swapped my pen for a pencile because I don't
+        take my writing seriously anymore.
+        All I wanna do is tell you what I tell you every time
+        I see you, 
+        But in a way that makes it special every time I do.
+        ''')
+    def tlinmw_poem(self):
+        print('''
+        You have always, been the light in my world,
+        You have been the essence of the earth that keeps me
+        around and alive,
+        You provide me with the requirements necessary to build
+        a life,
+        Most of all, you're the only source of hope that is
+        yet to extinguish.
+        You have a bad day but you push it under the rug
+        You bury your head inside of a book and you do whatever
+        it takes to make the bad day, worth it.
+        You help me out of my shell, you helped me out of the
+        prison that I was forced in to
+        by the people who were afraid of me,
+        You, you were the key to the door,
+        You are the future of my being.
+
+        I know you shouldn't be the dependancy of my happiness,
+        You shouldn't have more weight and more worlds on your
+        shoulders,
+        But I wanted to let you know that without you,
+        There would be no fire, there would be no gentle breeze
+        in the Summer.
+        I don't regret a thing about us, other than the fact we 
+        can't spend more time together.
+        I love you infinity,
+        I'll be with you for always,
+        Just as the sun never sets, never will the feelings I have
+        for you, deplete into nothing.
+    	''')
+    def tlinmw(self):
+        return ('''
+        You have always, been the light in my world,
+        You have been the essence of the earth that keeps me
+        around and alive,
+        You provide me with the requirements necessary to build
+        a life,
+        Most of all, you're the only source of hope that is
+        yet to extinguish.
+        You have a bad day but you push it under the rug
+        You bury your head inside of a book and you do whatever
+        it takes to make the bad day, worth it.
+        You help me out of my shell, you helped me out of the
+        prison that I was forced in to
+        by the people who were afraid of me,
+        You, you were the key to the door,
+        You are the future of my being.
+
+        I know you shouldn't be the dependancy of my happiness,
+        You shouldn't have more weight and more worlds on your
+        shoulders,
+        But I wanted to let you know that without you,
+        There would be no fire, there would be no gentle breeze
+        in the Summer.
+        I don't regret a thing about us, other than the fact we 
+        can't spend more time together.
+        I love you infinity,
+        I'll be with you for always,
+        Just as the sun never sets, never will the feelings I have
+        for you, deplete into nothing.
+    	''')
+    def iw_poem(self):
+        print('''
+        I close my eyes,
+        wake up in the morning, before I realise,
+        You're still here, 
+        I thought you left forever, but you're still my dear.
+        I just can't believe,
+        That I have you, in my arms,
+        I wanna hold you close,
+        maybe for a lifetime - never let you go,
+        I love you, 
+        tears hit my cheek, but I know it to stand true,
+        I just can't believe, 
+        that I have you, in my arms!
+    
+        I'll wait up,
+        I understand your work is so challenging,
+        But I'll still miss you,
+        Waiting for the phone is like an eternity,
+        I want to believe,
+        You'll let me hold you, in my arms,
+        I wanna hold your hand,
+        Just for a moment, but I see the crack,
+        I love you,
+        But all you do is smile and never say it back,
+        I want to believe,
+        You'll let me hold you, in my arms!
+        ''')
+    def iw(self):
+        return ('''
+        I close my eyes,
+        wake up in the morning, before I realise,
+        You're still here, 
+        I thought you left forever, but you're still my dear.
+        I just can't believe,
+        That I have you, in my arms,
+        I wanna hold you close,
+        maybe for a lifetime - never let you go,
+        I love you, 
+        tears hit my cheek, but I know it to stand true,
+        I just can't believe, 
+        that I have you, in my arms!
+
+        I'll wait up,
+        I understand your work is so challenging,
+        But I'll still miss you,
+        Waiting for the phone is like an eternity,
+        I want to believe,
+        You'll let me hold you, in my arms,
+        I wanna hold your hand,
+        Just for a moment, but I see the crack,
+        I love you,
+        But all you do is smile and never say it back,
+        I want to believe,
+        You'll let me hold you, in my arms!
+        ''')
+
+    def iwynrh_poem(self):
+        print ('''
+        It's a matter of minutes, since I saw you,
+        Desperate? Yes.
+        The scent of your fabric softener still
+        caresses my senses, beautiful.
+        Your smile is still fresh in my mind,
+        those sweet words you whispered
+        in my ear,
+        I miss you too.
+        ''')
+
+    def iwynrh(self):
+        return '''
+        It's a matter of minutes, since I saw you,
+        Desperate? Yes.
+        The scent of your fabric softener still
+        caresses my senses, beautiful.
+        Your smile is still fresh in my mind,
+        those sweet words you whispered
+        in my ear,
+        I miss you too.
+        '''
+
+    def nasano_poem(self):
+        print ('''
+        I couldn't find the pain to write, the voice to sing,
+        I hear the ringing of my phone and I know it's him,
+        The herald of failure, knocking at my door step,
+        Flexing his muscles, but I know I'm getting a slow death,
+        I left it too late - I always knew,
+        I'd this was never what I was meant to do,
+        The ink on my paper seems to dry out, as
+        the voices in my head are dying out,
+        The silence is deafening, the screams all perverted,
+        Whispers in the dark - how was it, I deserve this?
+        ''')
+
+    def nasano(self):
+        return '''
+        I couldn't find the pain to write, the voice to sing,
+        I hear the ringing of my phone and I know it's him,
+        The herald of failure, knocking at my door step,
+        Flexing his muscles, but I know I'm getting a slow death,
+        I left it too late - I always knew,
+        I'd this was never what I was meant to do,
+        The ink on my paper seems to dry out, as
+        the voices in my head are dying out,
+        The silence is deafening, the screams all perverted,
+        Whispers in the dark - how was it, I deserve this?
+        '''
+
+
+
+
+    def poem_choice(firstPoem):
+       userChoice = input("Please select a poem (1-5) \n1 - I don't know what to say \n2 - The light in my world \n3 - I Wanna \n4 - I Want You Here \n5 - Not As Strong As An Oak \nOr press 0 to exit \n")
+       userChoice = int(userChoice)
+       while userChoice != 0:
+            if userChoice == 1:
+                firstPoem.idkwts_poem()
+                firstPoem.save_idkwts(firstPoem)
+                
+                
+            if userChoice == 2:
+                firstPoem.tlinmw_poem()
+                firstPoem.save_tlinmw(firstPoem)
+                
+            
+            if userChoice == 3:
+                firstPoem.iw_poem()
+                firstPoem.save_iw(firstPoem)
+                
+            
+            if userChoice == 4:
+                firstPoem.iwynrh_poem()
+                firstPoem.save_iwynrh(firstPoem)
+                
+
+            if userChoice == 5:
+                firstPoem.nasano_poem()
+                firstPoem.save_nasano(firstPoem)
+                
+
+            print("\n")
+            userChoice = firstPoem.poem_choice()
+
+    def save_idkwts(self, firstPoem):
+        savePoem = input("Would you like to save the poem to a text file? Yes or no?\n")
+        while savePoem == "yes" or savePoem == "Yes":
+            f = open("I Don't Know What To Say.doc", "w")
+            f.write(firstPoem.idkwts())
+            f.close()
+            break
+
+    def save_tlinmw(self, firstPoem):
+        savePoem = input("Would you like to save the poem to a text file? Yes or no?\n")
+        while savePoem == "yes" or savePoem == "Yes":
+            f = open("The Light In My World.doc", "w")
+            f.write(firstPoem.tlinmw())
+            f.close()
+            break
+
+    def save_iw(self, firstPoem):
+        savePoem = input("Would you like to save the poem to a text file? Yes or no?\n")
+        while savePoem == "yes" or savePoem == "Yes":
+            f = open("I Wanna.doc", "w")
+            f.write(firstPoem.iw())
+            f.close()
+            break
+
+    def save_iwynrh(self, firstPoem):
+        savePoem = input("Would you like to save the poem to a text file? Yes or no?\n")
+        while savePoem == "yes" or savePoem == "Yes":
+            f = open("I Want You Here.doc", "w")
+            f.write(firstPoem.iwynrh())
+            f.close()
+            break
+
+    def save_nasano(self, firstPoem):
+        savePoem = input("Would you like to save the poem to a text file? Yes or no?\n")
+        while savePoem == "yes" or savePoem == "Yes":
+            f = open("Not As Strong As An Oak.doc", "w")
+            f.write(firstPoem.nasano())
+            f.close()
+            break
+        
